a038b85ca3eac17009df8633926a3e809a90ef5f
anavarros <anavarros625@gmail.com>
2024-04-03 14:53:24 +0200

    Se ha cambiado el header para que sea responsive ademas de tener ya todas las paginas terminadas

--- resources/views/departamentos_empleados/ver_empleados_departamentos.blade.php
+++ resources/views/departamentos_empleados/ver_empleados_departamentos.blade.php
@@ -1,30 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="/js/scriptDepartamentoEmpleados.js" async></script>
-    <link rel="stylesheet" href="/css/estilos.css">
-    <title>Document</title>
-    <style>
-        .departamentosEmpleadosHidden {
-            display: none;
-        }
-
-        li {
-            margin: 10px;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <div class="flex-container">
-            <div class="flex-item justify-start"><a href="{{@route('inicio')}}"><img src="/img/icono_home.png" alt="Icono de una casa" width="50px"></a></div>
-            <div class="flex-item"><a href="{{@route('empleados')}}">Empleados</a></div>
-            <div class="flex-item"><a href="{{@route('departamentos')}}">Departamentos</a></div>
-            <div class="flex-item"><a href="{{@route('departamentos_empleados')}}">Empleados departamentos</a></div>
-        </div>
+@extends('../templates/template')
+@section('content')
         <div class="sub-header">
             <div class="breadcrumbs"> Inicio > Empleados departamentos > Ver todos los empleados departamentos</div>
             <div class="options">
@@ -44,8 +19,4 @@
             <p id="listaDepartamentosEmpleados"></p>
         </div>
     </div>
-    <div class="footer">
-        <div class="footer-content">Hecho por Alberto Navarro | <a href="https://github.com/anavarros/cysnetCRUD">Repositorio de github</a></div>
-    </div>
-</body>
-</html>
+    @endsection
